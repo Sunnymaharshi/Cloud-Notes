3d5fceb4c4e3af3ce929f65d4c8b182dddd4662a
maharshi-deloitte <tanreddy@deloitte.com>
2023-09-03 16:01:55 +0530

    changes

--- aws sa asso/text_to_md.py
+++ aws sa asso/text_to_md.py
@@ -1,11 +1,16 @@
 import os, glob,re
-def change_font(text,size=3,tabs=1):
+def change_font(text,tabs):
     enclose = ''
-    # if size>=5:
-    #     enclose = "**"
+    
+    colors = ['#9400D3','#4B0082','#0000FF','#00FF00','#FFFF00','#FF7F00','#FF0000']
+    size = 7-tabs
+    if tabs>6:
+        tabs = 6
+    color = colors[6-tabs]
     if size<4:
         size = 4
-    return f"<font size='{str(size)}'>{(tabs*8)*'&nbsp;' + enclose+text+enclose}</font>"
+    
+    return f"""{(tabs*8)*'&nbsp;'}<font style='background:{color+'30'}' size='{str(size)}'>{enclose+text+enclose}</font>"""
 for filename in glob.glob('*.txt'):
     with open(os.path.join(os.getcwd(), filename), 'r') as f:
         md_text = ""
@@ -18,10 +23,9 @@ for filename in glob.glob('*.txt'):
                 text = line.rstrip()
                 by_space = len(re.findall(r'\s\s\s\s',text))
                 by_tab = len(re.findall(r'\t',text))
-                tabs = by_space if by_space>by_tab else by_tab
-                size = 7-tabs
+                tabs = by_space if by_space>by_tab else by_tab                
                 md_line = text.strip() 
-                md_line = change_font(md_line,size,tabs)
+                md_line = change_font(md_line,tabs)
                 md_text += md_line + "<br>"
         md_file = 'README.md'
         
