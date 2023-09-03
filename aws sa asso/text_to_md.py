088baa27ff196138b46b64816fa3874c392c2aaa
maharshi-deloitte <tanreddy@deloitte.com>
2023-09-03 15:06:26 +0530

    changes

--- aws sa asso/text_to_md.py
+++ aws sa asso/text_to_md.py
@@ -22,7 +22,7 @@ for filename in glob.glob('*.txt'):
                 size = 7-tabs
                 md_line = (tabs*8)*'&nbsp;' + text.strip() 
                 md_line = change_font(md_line,size)
-                md_text += md_line + "\n"
+                md_text += md_line + "<br>"
         md_file = 'README.md'
         
 
