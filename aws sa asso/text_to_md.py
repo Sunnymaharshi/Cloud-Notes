ce3cf74c344586a41743f757efeb498ca82d460d
maharshi-deloitte <tanreddy@deloitte.com>
2023-09-03 15:22:06 +0530

    changes

--- aws sa asso/text_to_md.py
+++ aws sa asso/text_to_md.py
@@ -5,7 +5,7 @@ def change_font(text,size=3,tabs=1):
     #     enclose = "**"
     if size<3:
         size = 3
-    return f"<font size='{str(size)}'>{(tabs*8)*'&nbsp;' + enclose+text+enclose}</font>".replace('\"','**')
+    return f"<font size='{str(size)}'>{(tabs*8)*'&nbsp;' + enclose+text+enclose}</font>"
 for filename in glob.glob('*.txt'):
     with open(os.path.join(os.getcwd(), filename), 'r') as f:
         md_text = ""
@@ -19,7 +19,7 @@ for filename in glob.glob('*.txt'):
                 by_space = len(re.findall(r'\s\s\s\s',text))
                 by_tab = len(re.findall(r'\t',text))
                 tabs = by_space if by_space>by_tab else by_tab
-                size = 7-tabs*2
+                size = 7-tabs*3
                 md_line = text.strip() 
                 md_line = change_font(md_line,size,tabs)
                 md_text += md_line + "<br>"
